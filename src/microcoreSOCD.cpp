ae660a005a852aa31c26e74ae89c67048f25c85b
AshenColors <3249074+AshenColors@users.noreply.github.com>
2021-06-01 18:31:24 -0400

    Implemented setting change structure and refactored

--- src/microcoreSOCD.cpp
+++ src/microcoreSOCD.cpp
@@ -6,129 +6,185 @@
  */
 
 #include <Arduino.h>
+//#include <RingEEPROM.h>
 
-#define LIP_SWITCH PB2
+//Pin definitions
+#define IP_MODE_SWITCH PB2
 #define L_IN PB0
 #define R_IN PB1
 #define L_OUT PB3
 #define R_OUT PB4
 
-void simpleSOCD();
-void LIPSOCD();
-void blinkenlights();
-
-//stored by pin number, 2 is an error state that gives neutral
+//stored by input pin number, 2 is an error state that gives neutral
 byte initial_input = 2;
+byte input_priority = 2; //0 = L_IN priority, 1 = R_IN priority, 2 = neutral, 3 = last input priority
+volatile byte button_pressed = 0;
+
+
+void SOCD(); //main socd-handling function
+void showIPmode(); //Reads from EEPROM and displays whatever it is through a sequence of outputs.
+void configIPmode(); //does the configuration
+void buttonpress();
 
 void setup()
 {
   // initialize the LED pin as an output:
   pinMode(L_IN, INPUT_PULLUP);
   pinMode(R_IN, INPUT_PULLUP);
-  // not in use yet
-  // pinMode(LIP_SWITCH, INPUT_PULLUP);
+  pinMode(IP_MODE_SWITCH, INPUT_PULLUP);
 
   // initialize the pushbutton pin as an input:
   pinMode(L_OUT, OUTPUT);
   pinMode(R_OUT, OUTPUT);
+
+  //attach our interrupt
+  //attachInterrupt(digitalPinToInterrupt(IP_MODE_SWITCH), buttonpress, FALLING);
 }
 
 void loop()
 {
-  switch (digitalRead(LIP_SWITCH))
-  {
-  case LOW:
-    simpleSOCD();
-    break;
-  case HIGH:
-    LIPSOCD();
-    break;
-  }
+  //initiate configuration if we have a button press
+  // if (button_pressed)
+  // {
+  //   configIPmode();
+  //   button_pressed = 0;
+  // }
+
+  //call the right SOCD function
+  SOCD();
 }
 
-void simpleSOCD()
+//this handles everything
+//replaces simpleSOCD() and LIPSOCD()
+void SOCD()
 {
   byte leftRead = digitalRead(L_IN);
   byte rightRead = digitalRead(R_IN);
 
-  if (leftRead == rightRead)
+  //same for all input methods
+  if (leftRead == HIGH && rightRead == HIGH)
   {
     //output neutral
     digitalWrite(L_OUT, HIGH);
     digitalWrite(R_OUT, HIGH);
   }
-  else if (leftRead == LOW)
+  else if (leftRead == LOW && rightRead == LOW)
   {
-    // activate PCB (LED OFF):
-    digitalWrite(L_OUT, LOW);
-    digitalWrite(R_OUT, HIGH);
-  }
-  else if (rightRead == LOW)
-  {
-    digitalWrite(L_OUT, HIGH);
-    digitalWrite(R_OUT, LOW);
-  }
-}
-
-void LIPSOCD()
-{
-  byte leftRead = digitalRead(L_IN);
-  byte rightRead = digitalRead(R_IN);
-
-  if (leftRead == LOW && rightRead == LOW)
-  {
-    switch (initial_input)
+    // switch (input_priority)
+    switch(2)
     {
-    case 2: //undefined state from startup
+    case 0:
+    case 1:
+      //write low to the pin corresponding with input priority
+      digitalWrite(input_priority + 3, LOW);
+      digitalWrite((!input_priority) + 3, HIGH);
+      break;
+    case 2:
+      //neutral
       digitalWrite(L_OUT, HIGH);
       digitalWrite(R_OUT, HIGH);
       break;
-    case L_IN: //output the opposite of the initial input
-      digitalWrite(L_OUT, HIGH);
-      digitalWrite(R_OUT, LOW);
-      break;
-    case R_IN:
-      digitalWrite(L_OUT, LOW);
-      digitalWrite(R_OUT, HIGH);
-      break;
-    default:
-      blinkenlights();
+    case 3:
+      //LIP SOCD
+      switch (initial_input)
+      {
+      case L_IN:
+      case R_IN:
+        digitalWrite(initial_input + 3, HIGH);
+        digitalWrite((!initial_input) + 3, LOW);
+        break;
+      case 2:
+      default:
+        digitalWrite(L_OUT, HIGH);
+        digitalWrite(R_OUT, HIGH);
+        break;
+      }
     }
-  }
-  else if (leftRead == LOW)
-  {
-    // activate PCB (LED OFF):
-    digitalWrite(L_OUT, LOW);
-    digitalWrite(R_OUT, HIGH);
-    initial_input = L_IN;
-  }
-  else if (rightRead == LOW)
-  {
-    digitalWrite(L_OUT, HIGH);
-    digitalWrite(R_OUT, LOW);
-    initial_input = R_IN;
-  }
-  else if (leftRead == HIGH && rightRead == HIGH)
-  {
-    //output neutral
-    digitalWrite(L_OUT, HIGH);
-    digitalWrite(R_OUT, HIGH);
   }
   else
   {
-    for (byte i = 0; i <= 4; i++)
+    if (leftRead == 0)
     {
-      blinkenlights();
+      digitalWrite(L_OUT, LOW);
+      digitalWrite(R_OUT, HIGH);
+    } else {
+      digitalWrite(L_OUT, HIGH);
+      digitalWrite(R_OUT, LOW);
     }
+    //initial_input = leftRead;
   }
 }
 
-void blinkenlights()
+
+void buttonpress()
 {
-  digitalWrite(L_OUT, HIGH);
-  digitalWrite(R_OUT, LOW);
-  delay(100);
-  digitalWrite(L_OUT, LOW);
-  digitalWrite(R_OUT, HIGH);
-  delay(100);
+  button_pressed = 1;
 }
+
+void configIPmode()
+{
+  byte basetime = millis();
+  while (digitalRead(IP_MODE_SWITCH) == LOW)
+  {
+    //freeze until the switch is released, allowing pressing of buttons
+  }
+  if (millis() - basetime > 1000) //if held for more than a second
+  {                               //start real configuration
+    byte leftRead = digitalRead(L_IN);
+    byte rightRead = digitalRead(R_IN);
+    if (leftRead == HIGH && rightRead == HIGH) //no input buttons pressed, go to neutral
+      input_priority = 2;
+    else if (leftRead == LOW && rightRead == HIGH)
+      input_priority = 0;
+    else if (leftRead == HIGH && rightRead == LOW)
+      input_priority = 1;
+    else if (leftRead == LOW && rightRead == LOW)
+      input_priority = 3;
+    // byte writeBuffer[PARAM_PACKET_SIZE];
+    // writeBuffer[0] = input_priority;
+    // myeepRom.savePacket(writeBuffer);
+  }
+  showIPmode();
+}
+
+void showIPmode()
+{
+  switch (input_priority)
+  {
+  case 0:
+  case 1: //absolute IP
+    for (byte i = 0, t = 0; i < 6; i++, t = !t)
+    {
+      digitalWrite(input_priority + 3, t);
+      digitalWrite((!input_priority) + 3, HIGH);
+      delay(200);
+    }
+    break;
+  case 2: //neutral IP
+    for (byte i = 0; i < 3; i++)
+    {
+      digitalWrite(L_OUT, LOW);
+      digitalWrite(R_OUT, HIGH);
+      delay(200);
+      digitalWrite(L_OUT, HIGH);
+      digitalWrite(R_OUT, HIGH);
+      delay(200);
+      digitalWrite(L_OUT, HIGH);
+      digitalWrite(R_OUT, LOW);
+      delay(200);
+      digitalWrite(L_OUT, HIGH);
+      digitalWrite(R_OUT, HIGH);
+      delay(200);
+    }
+    break;
+  case 3: //last input priority
+    for (byte i = 0, t = 0; i < 6; i++, t = !t)
+    {
+      digitalWrite(input_priority + 3, t);
+      digitalWrite((!input_priority) + 3, !t);
+      delay(200);
+    }
+    break;
+  }
+}
+
