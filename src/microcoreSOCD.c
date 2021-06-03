df5fa4a6f0cd6fd29df7d9630e63e3250a6ca3b0
AshenColors <3249074+AshenColors@users.noreply.github.com>
2021-06-03 14:19:37 -0400

    Removed dependency on Arduion (pure C baby)

--- src/microcoreSOCD.c
+++ src/microcoreSOCD.c
@@ -1,12 +1,8 @@
-/*
- * LIPSOIC.c
- *
- * Created: 5/28/2021 10:54:07 PM
- * Author : ashen
- */
+// microcore SOCD cleaner
+// a project by ashen_blue
 
-#include <Arduino.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 // #include <eewl.h>
 
 //Pin definitions
@@ -20,83 +16,83 @@
 // #define BUFFER_LEN 50         // number of data blocks
 
 //stored by input pin number, 1 is an error state that gives neutral
-byte initial_input = 1;
-byte input_priority = 1; //0 = L_IN priority, 2 = R_IN priority, 1 = neutral, 3 = last input priority
-volatile byte button_pressed = 0;
+uint8_t initial_input = 1;
+uint8_t input_priority = 1; //0 = L_IN priority, 2 = R_IN priority, 1 = neutral, 3 = last input priority
+volatile uint8_t button_pressed = 0;
 
 // EEWL eepromIPconfig(input_priority, BUFFER_LEN, BUFFER_START);
 
-void SOCD(); //main socd-handling function
+void SOCD();         //main socd-handling function
 void configIPmode(); //does the configuration
-void buttonpress();
 
 void setup()
 {
-  // initialize the LED pin as an output:
-  // pinMode(L_IN, INPUT_PULLUP);
-  // pinMode(R_IN, INPUT_PULLUP);
-  // pinMode(IP_MODE_SWITCH, INPUT_PULLUP);
-  // initialize the pushbutton pin as an input:
-  // pinMode(L_OUT, OUTPUT);
-  // pinMode(R_OUT, OUTPUT);
+  // initialize output pins and input pins/pullups:
   DDRB = (1 << L_OUT) | (1 << R_OUT);
-  PORTB =  (1 << L_IN) | (1 << R_IN) | (1<<IP_MODE_SWITCH);
+  PORTB = (1 << L_IN) | (1 << R_IN) | (1 << IP_MODE_SWITCH);
 
   //set IP mode from eeprom, or force a fake button push on boot
   // if (!(eepromIPconfig.get(input_priority)))
   // {
-      button_pressed = 1;
+  button_pressed = 1;
   // }
 
   //attach our interrupt
-  attachInterrupt(digitalPinToInterrupt(IP_MODE_SWITCH), buttonpress, FALLING);
+  //configure interrupt to activate on falling edge
+  MCUCR |= 0b10;
+  MCUCR &= ~(0b01);
+  //enable interrupt on INT0, which is PB1, and enable global interrupt in status register
+  GIMSK |= (1 << 6);
+  sei();
 }
 
-void loop()
+ISR(INT0_vect)
 {
-  //initiate configuration if we have a button press
-  if (button_pressed)
+  button_pressed = 1;
+}
+
+int main()
+{
+  setup();
+
+  while (1)
   {
-    configIPmode();
+    //initiate configuration if we have a button press
+    if (button_pressed)
+    {
+      configIPmode();
+    }
+    SOCD();
   }
-  SOCD();
 }
 
 //this handles everything
 //replaces simpleSOCD() and LIPSOCD()
 void SOCD()
 {
-  byte leftRead = digitalRead(L_IN);
-  byte rightRead = digitalRead(R_IN);
+  uint8_t leftRead = (PINB >> L_IN) & 1;
+  uint8_t rightRead = (PINB >> R_IN) & 1;
 
   //same for all input methods
-  if (leftRead == HIGH && rightRead == HIGH)
+  if (leftRead == 1 && rightRead == 1)
   {
     //output neutral
     PORTB |= (1 << L_OUT) | (1 << R_OUT);
-    // digitalWrite(L_OUT, HIGH);
-    // digitalWrite(R_OUT, HIGH);
   }
-  else if (leftRead == LOW && rightRead == LOW)
+  else if (leftRead == 0 && rightRead == 0)
   {
     switch (input_priority)
     {
     case L_IN:
-      // digitalWrite(L_OUT, LOW);
-      // digitalWrite(R_OUT, HIGH);
       PORTB &= ~(1 << L_OUT);
       PORTB |= (1 << R_OUT);
       break;
     case R_IN:
-      // digitalWrite(L_OUT, HIGH);
-      // digitalWrite(R_OUT, LOW);
       PORTB |= (1 << L_OUT);
       PORTB &= ~(1 << R_OUT);
       break;
     case 1:
       //neutral
-      // digitalWrite(L_OUT, HIGH);
-      // digitalWrite(R_OUT, HIGH);
       PORTB |= (1 << L_OUT) | (1 << R_OUT);
       break;
     case 3:
@@ -122,13 +118,13 @@ void SOCD()
   {
     if (leftRead == 0)
     {
-      // digitalWrite(L_OUT, LOW);
-      // digitalWrite(R_OUT, HIGH);
+      // digitalWrite(L_OUT, 0);
+      // digitalWrite(R_OUT, 1);
       PORTB &= ~(1 << L_OUT);
       PORTB |= (1 << R_OUT);
       initial_input = L_IN;
-    } 
-    else if (rightRead == 0) 
+    }
+    else if (rightRead == 0)
     {
       PORTB |= (1 << L_OUT);
       PORTB &= ~(1 << R_OUT);
@@ -137,27 +133,29 @@ void SOCD()
   }
 }
 
-void buttonpress()
-{
-  button_pressed = 1;
-}
-
 void configIPmode()
 {
-  while (digitalRead(IP_MODE_SWITCH) == LOW)
+  do
   {
-    digitalWrite(L_OUT, digitalRead(L_IN));
-    digitalWrite(R_OUT, digitalRead(R_IN));
-  }
-  byte leftRead = digitalRead(L_IN);
-  byte rightRead = digitalRead(R_IN);
-  if (leftRead == HIGH && rightRead == HIGH) //no input buttons pressed, go to neutral
+    //copy the IN bits to the OUT bits
+    if ((PINB >> L_IN) & 1)
+      PORTB |= (1 << L_OUT);
+    else
+      PORTB &= ~(1 << L_OUT);
+    if ((PINB >> R_IN) & 1)
+      PORTB |= (1 << R_OUT);
+    else
+      PORTB &= ~(1 << R_OUT);
+  } while (!((PINB >> IP_MODE_SWITCH) & 1));
+  uint8_t leftRead = (PINB >> L_IN) & 1;
+  uint8_t rightRead = (PINB >> R_IN) & 1;
+  if (leftRead == 1 && rightRead == 1) //no input buttons pressed, go to neutral
     input_priority = 1;
-  else if (leftRead == LOW && rightRead == HIGH)
+  else if (leftRead == 0 && rightRead == 1)
     input_priority = L_IN;
-  else if (leftRead == HIGH && rightRead == LOW)
+  else if (leftRead == 1 && rightRead == 0)
     input_priority = R_IN;
-  else if (leftRead == LOW && rightRead == LOW)
+  else if (leftRead == 0 && rightRead == 0)
     input_priority = 3;
   // eepromIPconfig.put(input_priority);
   initial_input = 1;
